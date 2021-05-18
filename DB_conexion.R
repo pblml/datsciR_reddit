cd239c308b31447ab3dc08ce0c481412a4f2d89c
dianacgr <dcguzman.20@gmail.com>
2021-05-18 20:52:18 +0200

    Create DB_conexion.R

---
+++ DB_conexion.R
@@ -0,0 +1,51 @@
+library(mongolite)
+
+options(mongodb = list(
+  "host" = "cluster0.xi7w2.mongodb.net",
+  "username" = "dcgr",
+  "password" = "FOw9ACHyEdmUPtwv"
+))
+
+loadData <- function(databaseName,collectionName) {
+  # Connect to the database
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                "mongodb+srv://%s:%s@%s/%s",
+                options()$mongodb$username,
+                options()$mongodb$password,
+                options()$mongodb$host,
+                databaseName
+              ),
+              options = ssl_options(weak_cert_validation = TRUE))
+  # Read all the entries
+  data <- db$find()
+  data
+}
+
+
+saveData <- function(databaseName,collectionName,data) {
+  # Connect to the database
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                "mongodb+srv://%s:%s@%s/%s",
+                options()$mongodb$username,
+                options()$mongodb$password,
+                options()$mongodb$host,
+                databaseName
+              ),
+              options = ssl_options(weak_cert_validation = TRUE))
+  # Insert the data into the mongo collection
+  db$insert(data)
+}
+
+
+databaseName <- "reddit"
+collectionName <- "finance"
+
+#read data from test data
+finance <- read.csv(file = '/datasets/reddit_finance/finance/submissions_reddit.csv')
+#store information from csv in database
+saveData(databaseName,collectionName,finance)
+
+#load data from database 
+financedb <- loadData(databaseName,collectionName)
