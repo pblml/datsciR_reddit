ba6eca87cfbd529272b922439418c6711e49122b
dianacgr <dcguzman.20@gmail.com>
2021-06-26 19:00:24 +0200

    Aggregation queries and new plots for communities

--- DB_connection.R
+++ DB_connection.R
@@ -51,6 +51,63 @@ saveData <- function(databaseName,collectionName,data) {
 }
 
 
+getDeletedPost <- function(databaseName,collectionName) {
+  # Connect to the database
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                "mongodb+srv://%s:%s@%s/%s",
+                options()$mongodb$username,
+                options()$mongodb$password,
+                options()$mongodb$host,
+                databaseName
+              ),
+              options = ssl_options(weak_cert_validation = TRUE))
+  # Filter data
+  #data <- db$find('{"$or": [{"user": "[deleted]"},{"author": "[deleted]"}]}')
+  data <- db$aggregate('[{"$match":{"$or": [{"user": "[deleted]"},{"author": "[deleted]"}]}},
+                       {
+                            "$group": {
+                                "_id": {
+                                    "$substr": [
+                                        "$comm_date", 0, 10
+                                    ]
+                                }, 
+                                "total": {
+                                    "$sum": 1
+                                }
+                            }
+                        }
+                       ]')
+  #data <- db$find('{"user": "[delete]"}')
+  data
+}
+
+getPostPerDay <- function(databaseName,collectionName){
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                "mongodb+srv://%s:%s@%s/%s",
+                options()$mongodb$username,
+                options()$mongodb$password,
+                options()$mongodb$host,
+                databaseName
+              ),
+              options = ssl_options(weak_cert_validation = TRUE))
+  data <- db$aggregate('[
+          {
+              "$group": {
+                  "_id": {
+                      "$substr": [
+                          "$comm_date", 0, 10
+                      ]
+                  }, 
+                  "total": {
+                      "$sum": 1
+                  }
+              }
+          }
+      ]')
+}
+
 databaseName <- "reddit"
 collectionName <- "finance"
 
